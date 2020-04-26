d298bc2f64d0b20cb79057838cef8165fca72c61
杨波 <yangbo@szy.cn>
2020-04-26 18:17:54 +0800

    初始化koa项目

---
+++ app/app.ts
@@ -0,0 +1,20 @@
+import path from 'path'
+import Koa from 'koa'
+import KoaRouter from 'koa-router'
+import views from 'koa-views'
+import routes from './routes'
+const app = new Koa()
+const router = routes(new KoaRouter())
+
+const host = 'localhost'
+const port = 3000
+
+app.use(views(path.join(__dirname, './views'), {
+	extension: 'ejs'
+}))
+.use(router.routes())
+.use(router.allowedMethods())
+
+app.listen(port, () => {
+	console.log(`server is listen in http://${host}:${port}`)
+})
---
+++ ecosystem.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+	apps: [{
+		name: 'koa-app',
+		script: 'app/app.js',
+		args: 'null',
+		instances: 2, // 集群实例，可以只有一个，这样表现上与fork无异，但可以用scale
+		exec_mode: "cluster", // 集群模式，如不指定，默认为fork
+		autorestart: false,
+		min_uptime: "60s",
+		max_restarts: 3,
+		watch: false,
+		error_file: "./logs/app-err.log",
+		out_file: "./logs/app-out.log",
+		log: "./logs/app.log",
+		max_memory_restart: '1G',
+		kill_timeout: 3000,
+		env: {
+			NODE_ENV: 'development'
+		},
+		env_production: {
+			NODE_ENV: 'production'
+		}
+	}]
+}
