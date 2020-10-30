5e955528c26b8fea9f20423fd554ce2182d81a73
we_coder <5642431+we_coder@user.noreply.gitee.com>
2020-10-30 09:59:36 +0800

    update ecosystem.config.js.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,7 @@ module.exports = {
 		args: 'null',
 		instances: 2, // 集群实例，可以只有一个，这样表现上与fork无异，但可以用scale
 		exec_mode: 'cluster', // 集群模式，如不指定，默认为fork
-		autorestart: false,
+		autorestart: true,
 		min_uptime: '60s',
 		max_restarts: 3,
 		watch: false,
